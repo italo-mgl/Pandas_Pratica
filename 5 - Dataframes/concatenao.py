65e7cf6182220023f8bdaf23a98abfeffc2d2cc4
Itin <thenewera07@gmail.com>
2024-10-14 17:42:29 -0300

    perto de encerrar dia 5

--- 5 - Dataframes/concatenao.py
+++ 5 - Dataframes/concatenao.py
@@ -1,6 +1,25 @@
 #%%
 import pandas as pd
+import os
 #%%
-df = pd.read_csv("../data/ipea/homicidios.csv", sep=";")
+path = "../data/ipea/"
+files = os.listdir(path)
 
-df.rename(columns={"valor": "homicidios"})
+print(files)
+#%%
+
+def import_etl(path:str):
+    name = path.split("/")[-1].split(".")[0]
+    
+    df = (pd.read_csv(path, sep=";")
+            .rename(columns={"valor":name})
+            .set_index(["cod","nome","período"]))
+    return df
+
+#%%
+dfs = []
+for i in files:
+    dfs.append(import_etl(path+i))
+#%%
+df_final = pd.concat(dfs, axis=1).reset_index()
+df_final.to_csv("../data/dado-finais.csv", sep=";", index=False)
