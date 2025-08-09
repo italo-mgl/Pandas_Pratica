7f10bb2dbfd608508703eb5571971ea83fd83ec5
Itin <thenewera07@gmail.com>
2025-08-09 19:34:55 -0300

    iloc

--- dia-1/03_series_index.py
+++ dia-1/03_series_index.py
@@ -14,4 +14,27 @@ series_idades
 idades[-1]
 
 #%%
-series_idades[0]
+series_idades = series_idades.sort_values()
+series_idades
+#%%
+
+series_idades.loc[0]
+#%%
+
+series_idades.iloc[:3]
+#%%
+idades2 = [
+    25,25,65,39,25,63,32,42,54,31
+]
+
+indexs = [
+    "Teo","Nah","Itin","Francisco","Bartolomeu",
+    "Vic","Kozato","Andrei","Fernanda","Olavo",
+
+]
+# %%
+series_idades2 = pd.Series(idades2, index=indexs)
+series_idades2
+
+#%%
+series_idades2["Bartolomeu   "]
